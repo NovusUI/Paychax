4607da5b48b878b3e3164735643ac28917efcf6a
thenodenovice <thenodenovice@gmail.com>
2023-04-25 03:47:06 +0100

    update paychex

---
+++ script.js
@@ -0,0 +1,18 @@
+const menuBtn = document.querySelector(".menu-btn");
+const menu = document.querySelector("nav.menu");
+const paychexSmall = document.querySelector("#paychex-small")
+
+let menuOpen = false;
+menuBtn.addEventListener("click", () => {
+  if (!menuOpen) {
+    menuBtn.classList.add("active");
+    menu.style.display = "flex";
+    // paychexSmall.style.display= "none";
+    menuOpen = true;
+  } else {
+    menuBtn.classList.remove("active");
+    menu.style.display = "none";
+    // paychexSmall.style.display = "flex";
+    menuOpen = false;
+  }
+});
